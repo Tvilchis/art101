05f2ff707df97150cce28fd779fb510d594b9a61
Tvilchis <130083568+Tvilchis@users.noreply.github.com>
2023-04-24 22:12:21 -0700

    lab4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,19 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Tenshi Vilchis
+// Date: 4/24/23
 
-// Constants
+//Choose a random 4 letter word
+	//select word from a linked file with a list of 4 letter words
+//Print 4 spaces for the words, without showing the word
+//Print first image of gallows
+//User inputs a letter 
+	// If guessed correctly, correct letter is shown 
+	//  If guessed incorrectly, add 1 out of 6 limbs
+		//When a limb is added, move to the next image (6 images total)
+	// If letter has already been guessed, allow user to put in a different input
+		//Add incorrect letter to bottom of screen in alphabetical order
+//Evaluate win or lose after every letter’s conditions, if neither is met repeat user input phase
+//Win condition: fills all 4 letter slots correctly
+	//Show Win screen
+//Loose condition: guesses 6 letters wrong and all limbs have been imputed
+	//Show loose screen of a full body being hung 
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
